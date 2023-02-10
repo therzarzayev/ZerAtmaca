05fbe61341c7607616aa5136fc36b92dce6d4a31
therzarzayev <rrza3639@gmail.com>
2023-02-10 22:10:47 +0400

    Added shake event detection

--- app/src/main/java/com/therzarzayev/lifecycle/MainActivity.kt
+++ app/src/main/java/com/therzarzayev/lifecycle/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.therzarzayev.lifecycle
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import java.util.*
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +21,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dice1Img: ImageView
     private lateinit var dice2Img: ImageView
 
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("onCreate")
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager)!!.registerListener(
+            sensorListener,
+            sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_GAME
+        )
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
 
         button = findViewById(R.id.button)
         priceText = findViewById(R.id.textView)
@@ -31,6 +50,41 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
     }
+
+    ////////////////////////  Shake event detection  ///////////////////////////////////////////////////
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+            if (acceleration > 20) {
+                //Shake event codes
+                rollDice()
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(
+            sensorListener, sensorManager!!.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER
+            ), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
+    ////////////////////////  Shake event detection  ///////////////////////////////////////////////////
 
     @SuppressLint("SetTextI18n")
     fun rollDice() {
